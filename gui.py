feb5e6f49531dd6d8f11a741fd97f219497c6af0
Calvin Min <Calvinjmin@gmail.com>
2020-06-02 19:54:31 -0400

    VERSION 2.4
    Massive commit: rearranged the GUI layout and implemented a standardize system within gui.py.

--- gui.py
+++ gui.py
@@ -1,4 +1,5 @@
 # Reference for tkinter - https://realpython.com/python-gui-tkinter/
+
 from tkinter import *
 import money
 import portfolio
@@ -25,87 +26,122 @@ money_display = "\nThe following options are currently available (Type the numbe
                 "\nType 'return' to go back to the main options.\n"
 
 
-def represents_int(str):
-    try:
-        int(str)
-        return True
-    except ValueError:
-        return False
-
-
 class MainGUI:
     def __init__(self, master, money, portfolio, watch_list):
-        # 0 - Display, 1 - Portfolio Display, 2 - Money Display
-        self.type_display = 0
-
-        # Window
+        ### Window ###
         self.window = master
 
-        # Frame
+        ### Frame ###
         self.frame = Frame(master=master)
 
-        # Import VARS
+        ### Import VARS ###
         self.money = money
         self.port = portfolio
         self.wl = watch_list
 
-        # Widgets
-        self.intro_text = Label(text=intro).grid(row=0, column=0, sticky='nsew', columnspan=3)
+        ### Widgets ###
 
-        self.display_str = StringVar()
-        self.display_str.set(display)
-        self.init_options = Label(textvariable=self.display_str).grid(row=2, column=0, sticky='w', columnspan=1)
+        # - Row 0 - #
+        self.intro_text = Label(text=intro).grid(row=0, column=0, sticky='nsew', columnspan=5)
 
+        # - Row 1 - #
         self.funds_str = StringVar()
         self.funds_str.set("Current Funds: $" + str(money.get_funds()))
-        self.funds_text = Label(textvariable=self.funds_str).grid(row=1, column=0, sticky='nsew', columnspan=3)
+        self.funds_text = Label(textvariable=self.funds_str).grid(row=1, column=0, sticky='nsew', columnspan=5)
 
-        self.response_str = StringVar()
-        self.response_str.set("")
-        self.user_response = Entry(textvariable=self.response_str).grid(row=2, column=1, sticky='w')
+        # - Row 2 - #
+        self.type_display_str = StringVar()
+        self.type_display_str.set("Main Display")
+        self.type_display = Label(textvariable=self.type_display_str).grid(row=2, column=0, sticky='nsew', columnspan=5)
 
-        self.results_str = StringVar()
-        self.results_str.set("")
-        self.entry_button = Button(text="Enter", command=self.change_results).grid(row=2, column=2, sticky='w')
-        self.results_label = Label(textvariable=self.results_str).grid(row=3, column=0, sticky='nsew', columnspan=3)
+        # - Row 3 - #
+        # Main Menu Button
+        self.reset_button = Button(text="Main Menu", command=self.reset_gui)
+        self.reset_button.grid(row=3, column=0)
+
+        # Portfolio Button
+        self.portfolio_button = Button(text="Portfolio")
+        self.portfolio_button.grid(row=3, column=1)
+
+        # Funds Button
+        self.funds_button = Button(text="Funds")
+        self.funds_button.grid(row=3, column=2)
+
+        # Watch List Button
+        self.wl_button = Button(text="Watch Lists")
+        self.wl_button.grid(row=3, column=3)
+
+        # Stock Info. Button
+        self.stock_info_button = Button(text="Indv. Stock")
+        self.stock_info_button.grid(row=3, column=4)
+
+        # - Row 4 - #
+        # Portfolio Label
+        self.port_str = StringVar()
+        self.port_str.set("Manipulate your the portfolio.")
+        self.port_message = Label(textvariable=self.port_str)
+        self.port_message.grid(row=4, column=1)
+
+        # Funds Label
+        self.funds_str = StringVar()
+        self.funds_str.set("Access options to your funds.")
+        self.funds_message = Label(textvariable=self.funds_str)
+        self.funds_message.grid(row=4, column=2)
+
+        # Watch List Label
+        self.wl_str = StringVar()
+        self.wl_str.set("Manipulate your watch lists.")
+        self.wl_message = Label(textvariable=self.wl_str)
+        self.wl_message.grid(row=4, column=3)
+
+        # Stock Info. Label
+        self.stock_info_str = StringVar()
+        self.stock_info_str.set("Access info. about a single stock.")
+        self.stock_info_message = Label(textvariable=self.stock_info_str)
+        self.stock_info_message.grid(row=4, column=4)
+
+        ### Option Widgets ###
+        ## UPCOMING CHANGES
 
     def set_window_title(self, title):
         self.window.title(title)
 
-    def change_results(self):
-        if self.type_display == 0:
-            self.update_display()
-        elif self.type_display == 2:
-            self.update_funds()
+    def hide_main_widgets(self):
+        self.portfolio_button.grid()
+        self.port_message.grid_remove()
 
+        self.funds_button.grid_remove()
+        self.funds_message.grid_remove()
+
+        self.wl_button.grid_remove()
+        self.wl_message.grid_remove()
+
+        self.stock_info_button.grid_remove()
+        self.stock_info_message.grid_remove()
+
+    def reset_gui(self):
+        self.type_display_str.set("Main Display")
+
+        # Restore Buttons and Text
+        self.portfolio_button.grid()
+        self.port_message.grid()
+
+        self.funds_button.grid()
+        self.funds_message.grid()
+
+        self.wl_button.grid()
+        self.wl_message.grid()
+
+        self.stock_info_button.grid()
+        self.stock_info_message.grid()
+
+    def port_options(self):
+        self.hide_main_widgets()
+
+    def update_funds(self):
         # Updates the Current Funds str
         text = "Current Funds: $" + str(self.money.get_funds())
         self.funds_str.set(text)
-
-        # Updates the result/response str
-        self.results_str.set("Option Selected - " + str(self.response_str.get()))
-        self.response_str.set("")
-
-    def update_display(self):
-        if self.response_str.get() == '1' or 'Manipulate Portfolio':
-            self.display_str.set(portfolio_Display)
-        elif self.response_str.get() == '2' or 'Manipulate Funds':
-            self.display_str.set(money_display)
-        else:
-            self.display_str.set(display)
-
-    def update_funds(self):
-
-        """"
-        if self.response_str.get() == '1' or 'Deposit Money':
-            while True:
-                if represents_int(self.response_str.get()):
-                    self.money.deposit(int(self.response_str.get()))
-                    self.display_str.set(display)
-                    self.type_display = 0
-        """
-
-
 
 
 def main(money, port, watchlist):
@@ -113,22 +149,14 @@ def main(money, port, watchlist):
     window.grid_rowconfigure(0, weight=1)
     window.grid_rowconfigure(1, weight=1)
     window.grid_rowconfigure(2, weight=1)
+    window.grid_rowconfigure(3, weight=1)
+    window.grid_rowconfigure(4, weight=1)
     window.grid_columnconfigure(0, weight=1)
     window.grid_columnconfigure(1, weight=1)
+    window.grid_columnconfigure(2, weight=1)
+    window.grid_columnconfigure(3, weight=1)
+    window.grid_columnconfigure(4, weight=1)
 
     gui = MainGUI(window, money, port, watchlist)
     gui.set_window_title("STOCKS")
     window.mainloop()
-
-"""    def add_label_with_pos(self, text, row, column, pos, col_span):
-        label = Label(text=text)
-        label.grid(row=row, column=column, sticky=pos, columnspan=col_span)
-
-    def add_button_with_pos(self, text, row, column, pos):
-        button = Button(text=text)
-        button.grid(row=row, column=column, sticky=pos)
-
-    def add_entry_with_pos(self, default_text, row, column, pos):
-        entry = Entry(text=default_text)
-        entry.grid(row=row, column=column, sticky=pos) """
-
